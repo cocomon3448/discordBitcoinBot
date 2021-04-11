c8f577755d608d44947d0793462a3284ec62b46a
cocomon3448 <73870100+cocomon3448@users.noreply.github.com>
2021-04-11 13:09:34 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -151,13 +151,7 @@ async def ping(ctx):
         statu = 'Slow'
     await ctx.send(f'Ping is {ping} ms ({statu})')
 
-# @client.command()
-# async def kick(ctx, member : discord.Member,*,reason=None):
-#     await member.kick(reason=reason)
-# @client.command()
-# async def ban(ctx, member : discord.Member,*,reason=None):
-#     await member.ban(reason=reason)
-#     await ctx.send(f'Banned {member.mention}')
+
 ###################################################################
 current_gives = 10
 def editasset(username, current_asset):
